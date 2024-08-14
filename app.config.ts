c53363fa1faf34fc82c2e158bc456b82ba9224d8
The Dragonaut <srodriki@gmail.com>
2024-08-14 15:40:43 -0300

    make it work with cf-workers and wrangler

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-  middleware: './src/middleware'
+  middleware: './src/middleware',
+  server: {
+    preset: 'cloudflare-pages',
+    rollupConfig: {
+      external: ['__STATIC_CONTENT_MANIFEST', 'node:async_hooks']
+    }
+  },
 });
